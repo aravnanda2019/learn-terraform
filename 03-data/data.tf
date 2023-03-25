8987f84e679f66cbb30a7ff9768bdfa5068fb08e
aravnanda2019 <aravnanda2019@gmail.com>
2023-03-25 12:10:55 +0530

    updating ./learn-terraform

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,8 +1,9 @@
-data "aws_ami" "example" {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["858642391731"]
 }
+
 output "ami_id" {
-  value=data.aws_ami.image_id
+  value = data.aws_ami.ami.image_id
 }
